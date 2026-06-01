dfac5bb6bcf13c477a1c39ed135b26cb9f17b100
newby-by <practikum10@gmail.com>
2026-06-01 19:21:13 +0300

    Add test create route

---
+++ data.py
@@ -0,0 +1,3 @@
+BASE_URL = "https://qa-routes.education-services.ru/"
+
+ADDRESSES = ["улица Хамовнический Вал, 34", "Зубовский бульвар, 37", ]
---
+++ pages/__init__.py
@@ -0,0 +1,1 @@
+from .create_route_page import CreateRoutePage
